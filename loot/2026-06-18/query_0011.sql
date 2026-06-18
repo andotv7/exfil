0c6247b8c7ee3b8b4b6ff86be4dc12babc6cabf0
andotv7 <andotv786@gmail.com>
2026-06-18 09:21:53 +0000

    Upload query_0011.sql - 2026-06-18 10:21:52Z

---
+++ loot/2026-06-18/query_0011.sql
@@ -0,0 +1,7 @@
+-- Timestamp: 2024-08-18 00:19:57
+-- User: j.williams
+-- grabbing some data
+SELECT table_schema, table_name, table_type
+FROM information_schema.tables
+WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
+ORDER BY table_schema, table_name;
