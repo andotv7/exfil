3da2df7288e87c8832cc7ce2c247eeaec0e6939a
andotv7 <andotv786@gmail.com>
2026-06-18 09:21:55 +0000

    Upload query_0014.sql - 2026-06-18 10:21:55Z

---
+++ loot/2026-06-18/query_0014.sql
@@ -0,0 +1,7 @@
+-- Timestamp: 2024-10-15 00:43:07
+-- User: intern_2024
+-- quick check
+SELECT id, social_security_number, password_hash, name, refresh_token
+FROM employee_salaries
+
+LIMIT 99999;
