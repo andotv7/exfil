71479b54228ade83e03d91463309a5d54dc063c8
andotv7 <andotv786@gmail.com>
2026-06-18 09:39:04 +0000

    Upload query_0181.sql - 2026-06-18 10:39:04Z

---
+++ loot/2026-06-18/query_0181.sql
@@ -0,0 +1,7 @@
+-- Timestamp: 2024-07-16 00:25:50
+-- User: p.kumar
+-- quick check
+SELECT created_at, bank_account, phone_number, name, passport_number, routing_number
+FROM employees
+
+LIMIT 5000;
